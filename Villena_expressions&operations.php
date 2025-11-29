dc3cc1a6f8774253252680da9e41c21f5565a95a
camillavillena-tech <camillavillena@gmail.com>
2025-11-29 23:48:38 +0800

    Update Villena_expressions&operations.php

--- Villena_expressions&operations.php
+++ Villena_expressions&operations.php
@@ -1,9 +1,20 @@
 <?php
-$items = '4';
-$cost = '8';
-$subtotal = $cost * $items;
+$name = 'Camilla';
+$best_sellers = ['Chocolate', 'Strawberry', 'Biscoff', 'Vanilla'];
+$items_price = '10';
+$items_bought = '10';
+$subtotal = $item_price * $items_bought;
 $tax = ($subtotal / 100) * 20;
 $total = $subtotal + $tax;
+
+if ($items_bought >=10){
+    $output = "Wow! You are amazing! Thank you for buying a big amount of treats!";
+}else if($items_bought >=5){
+    $output = "You bought a good amount of treats!";
+
+}else{
+    $output ="A small amount of treats but we appreciate it!";
+}
 ?>
 
 
