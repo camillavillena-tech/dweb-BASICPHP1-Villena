d92c7712eb333505012c857c4f546a80a48c2b91
camillavillena-tech <camillavillena@gmail.com>
2025-11-29 23:42:40 +0800

    Add header php

--- Villena_expressions&operations.php
+++ Villena_expressions&operations.php
@@ -6,13 +6,7 @@ $tax = ($subtotal / 100) * 20;
 $total = $subtotal + $tax;
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-    <title>Expressions & Operations</title>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="css/styles.css">
-</head>
+
 <body>
     <h1>The Candy Store</h1>
     <h2>Shopping Cart</h2>
---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>The Candy Store</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="css/styles.css">
+</head>
